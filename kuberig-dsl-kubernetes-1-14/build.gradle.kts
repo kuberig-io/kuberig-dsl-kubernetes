f343a4bb8bc2a3c5f3594524be8cbe30619e98b5
Tom Eyckmans <teyckmans@gmail.com>
2019-04-09 20:46:24 +0200

    initial commit

---
+++ kuberig-dsl-kubernetes-1-14/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("eu.rigeldev.kuberig.dsl.generator") version "0.0.8"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    val testImplementation by configurations
+    val testRuntimeOnly by configurations
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
+}
